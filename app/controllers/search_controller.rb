9d05eedcc4459d93dce551c4a00306a8182c57c2
Alax Alves <alaxallves@gmail.com>
2019-09-25 16:43:15 -0300

    Matching query by a single 'search param'

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,13 @@ class SearchController < ApplicationController
   def search
     query = params[:query]
     pp params
-    find_query = Profile.where("name ILIKE ? OR username ILIKE ? OR description ILIKE ?", "%#{query}%","%#{query}%","%#{query}%")
+    find_query = Profile.where("name ILIKE ? OR
+                                username ILIKE ? OR
+                                description ILIKE ?",
+                               "%#{query}%",
+                               "%#{query}%",
+                               "%#{query}%"
+                              ).uniq
     search_results = find_query
     render json: search_results
   end
